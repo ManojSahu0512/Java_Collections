af38aa8e9f2c01d6cbe2555ba0ceb49bc48cd1d9
Manoj Sahu <sahumanoj0512@gmail.com>
2025-05-06 09:23:12 +0530

    Java 8 features added

---
+++ src/Java8Features/DefaultAndStaticMethodsInInterfaces/Default/TestDefault.java
@@ -0,0 +1,21 @@
+package Java8Features.DefaultAndStaticMethodsInInterfaces.Default;
+interface Vehicle{
+    void start();
+    default void fuelType(){
+        System.out.println("Use petrol or diesel");
+    }
+}
+class Car implements Vehicle{
+    public void start(){
+        System.out.println("Car is starting...");
+    }
+}
+
+public class TestDefault {
+    public static void main(String[] args) {
+        Car car=new Car();
+        car.start();//Output: Car is starting...
+        car.fuelType();//Output: Uses Petrol or Diesel
+
+    }
+}
---
+++ src/Java8Features/DefaultAndStaticMethodsInInterfaces/Default/TransactionApplication.java
@@ -0,0 +1,34 @@
+package Java8Features.DefaultAndStaticMethodsInInterfaces.Default;
+
+interface TransactionLogger {
+    // Abstract method to implement in service
+    void logTransaction(String txnId, double amount);
+
+    // Default method – common reusable behavior
+    default void logAudit(String msg) {
+        System.out.println("[AUDIT] " + msg);
+    }
+        // Static method – utility for formatting
+        static String formatCurrency (double amount){
+            return String.format("₹%.2f", amount);
+        }
+
+    }
+    class PaymentTransactionService implements TransactionLogger{
+
+        @Override
+        public void logTransaction(String txnId, double amount) {
+            String formatted = TransactionLogger.formatCurrency(amount);
+            System.out.println("Transaction ID: " + txnId + ", Amount: " + formatted);
+            logAudit("Transaction logged at " + java.time.LocalDateTime.now());
+        }
+    }
+
+    public class TransactionApplication {
+        public static void main(String[] args) {
+            PaymentTransactionService service=new PaymentTransactionService();
+            service.logTransaction("TXN12345", 15000.50);
+            // You can also call the static method independently
+            System.out.println("Test format: " + TransactionLogger.formatCurrency(500.75));
+        }
+    }
